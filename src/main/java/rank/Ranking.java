2505b6e8c85836c3397871dcde700a16239b142a
WenTr <tran.wendy7@gmail.com>
2015-03-19 18:57:50 -0700

    added addedRank

--- src/main/java/rank/Ranking.java
+++ src/main/java/rank/Ranking.java
@@ -1,6 +1,7 @@
 package rank;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -57,28 +58,28 @@ public class Ranking {
 				for (CrawledLink everyLink : linkers) {
 					if(eachLink != everyLink){
 						for (Link perLink : everyLink.getListOfLinks()) {
-	
+
 							String a = eachLink.getLinkURL().toString();
 							if (a.lastIndexOf("/") == a.length() - 1
 									|| a.lastIndexOf("#") == a.length() - 1) {
 								a = StringUtils.stripEnd(a, "/");
 								a = StringUtils.stripEnd(a, "#");
 							}
-	
+
 							String b = perLink.getUrl().toString();
 							if (b.lastIndexOf("/") == b.length() - 1
 									|| b.lastIndexOf("#") == b.length() - 1) {
 								b = StringUtils.stripEnd(b, "/");
 								b = StringUtils.stripEnd(b, "#");
 							}
-							
+
 							String c = everyLink.getLinkURL().toString();
 							if (c.lastIndexOf("/") == c.length() - 1
 									|| c.lastIndexOf("#") == c.length() - 1) {
 								c = StringUtils.stripEnd(c, "/");
 								c = StringUtils.stripEnd(c, "#");
 							}
-							
+
 							if (a.equals(b) && !aa.contains(c)) {
 								//System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7: " + c);
 								aa.add(c);
@@ -86,30 +87,30 @@ public class Ranking {
 						}
 					}
 				}
-				
-//				System.out.println("LINKS SIZE" + links.size());
-				
+
+				//				System.out.println("LINKS SIZE" + links.size());
+
 				float h = (float) 0;
 
-				
+
 				//for(int w = 0; w < linkers.size(); w++){
 				//	if()
 				//}
 				//System.out.println(aa.size());
 				for (int w = 0; w < aa.size(); w++) {
-					
+
 					if (oldranks.containsKey(aa.get(w))) {
 						//System.out.println(aa.get(w) + " : " + w);
 						// 
-						 int uy = 0;
-						 for(int p = 0; p < linkers.size(); p++){
-							 if(linkers.get(p).getLinkURL().equals(aa.get(w))){
-								 uy = p;
-								 break;
-							 }
-						 }
+						int uy = 0;
+						for(int p = 0; p < linkers.size(); p++){
+							if(linkers.get(p).getLinkURL().equals(aa.get(w))){
+								uy = p;
+								break;
+							}
+						}
 
-						 //System.out.println(aa.get(w) + "::" + linkers.get(uy).getLinkURL());
+						//System.out.println(aa.get(w) + "::" + linkers.get(uy).getLinkURL());
 						h += oldranks.get(aa.get(w)) / linkers.get(uy).getListOfLinks().size();
 					}
 				}
@@ -120,37 +121,64 @@ public class Ranking {
 		for (Entry<String, Float> entry : ranks.entrySet()) {
 			System.out.println(entry.getKey() + " : " + entry.getValue());
 		}
-		
+
 		return ranks;
 
 	}
 
-	public Map<String, Integer> addedRanking(Set<CrawledLink> allLinks) {
+	//rank by the number of words a document has
+	public Map<String, Double> addedRanking(Set<CrawledLink> allLinks) {
 
-		Map<String, Integer> urlNnumWords = new HashMap<String, Integer>();
+		Map<String, Integer> tempMap = new HashMap<String, Integer>();
+		Map<String, Double> urlNnumWords = new HashMap<String, Double>();
 		//Queue<Map.Entry<String, Integer>> arrangedWL = new LinkedList<Map.Entry<String, Integer>>();
 
 		for (CrawledLink link : allLinks) {
 			if (link.getWordSet() != null) {
-				urlNnumWords.put(link.getLinkURL(), link.getWordSet().size());
+				tempMap.put(link.getLinkURL(), link.getWordSet().size());
 			}
 		}
-		
 
+		//		for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
+		//			System.out.println(entry.getKey() + ": " + entry.getValue());
+		//		}
+		//		System.out.println(Collections.max(tempMap.values()));
 
-		for (Map.Entry<String, Integer> entry : urlNnumWords.entrySet()) {
+		for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
+			urlNnumWords.put(entry.getKey(), ((double)entry.getValue()/Collections.max(tempMap.values())));
+		}
+
+		for (Entry<String, Double> entry : urlNnumWords.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
-		
-		return urlNnumWords;
 
 
-		
+		/**
+			List<Integer> listVals = new ArrayList<Integer>(numWords.values());
+			Collections.sort(listVals);
+			Collections.reverse(listVals);
+
+			for (Object i : listVals) {
+				for (Map.Entry<String, Integer> entry : numWords.entrySet()) {
+					//System.out.println(entry.getKey() + ": " + entry.getValue());
+					if (entry.getValue().equals(i)) {
+						arrangedWL.add(entry);
+					}
+				}
+			}
+
+			for (Map.Entry<String, Integer> eachEntry : arrangedWL) {
+				System.out.println(eachEntry);
+			}
+		 **/
+
+
 		//For testing purposes 
 		//Storage save = new Storage("index.json", "ranking.json");
 		//save.storeRank(urlNnumWords);
-	}
 
+		return urlNnumWords;
+	}
 
 
 }
